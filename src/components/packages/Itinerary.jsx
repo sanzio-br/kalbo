ba31d93c53aa440d957ec5bf29de8176a4edb0a5
sanzio-br <briansanzii@gmail.com>
2022-07-29 11:59:13 +0300

    itinerary fix

--- src/components/packages/Itinerary.jsx
+++ src/components/packages/Itinerary.jsx
@@ -5,34 +5,34 @@ const Itinerary = ({ itinerary }) => {
       <div className="day-details">
         <div>
           {itinerary
-            ? itinerary.contentState ? 
+            ? itinerary.contentState ? itinerary.contentState.length ? 
             itinerary.contentState.blocks.map((block) => {
-                return (
-                  <div key={block.key}>
-                    <div>
-                      {block.inlineStyleRanges
-                        ? block.inlineStyleRanges.map((inlineStyleRange) => {
-                            return (
-                              <>
-                                {inlineStyleRange.style === "UNDERLINE" ? (
-                                  <div className="day-header">{block.text}</div>
-                                ) : (
-                                  ""
-                                )}
-                              </>
-                            );
-                          })
-                        : ""}
-                    </div>
-                    {!block.inlineStyleRanges.length &&
-                    block.type === "ordered-list-item" ? (
-                      <ol>{block.text}</ol>
-                    ) : (
-                      !block.inlineStyleRanges.length && block.text
-                    )}
+              return (
+                <div key={block.key}>
+                  <div>
+                    {block.inlineStyleRanges
+                      ? block.inlineStyleRanges.map((inlineStyleRange) => {
+                          return (
+                            <>
+                              {inlineStyleRange.style === "UNDERLINE" ? (
+                                <div className="day-header">{block.text}</div>
+                              ) : (
+                                ""
+                              )}
+                            </>
+                          );
+                        })
+                      : ""}
                   </div>
-                );
-              }) : <div  dangerouslySetInnerHTML={{__html: itinerary}} />
+                  {!block.inlineStyleRanges.length &&
+                  block.type === "ordered-list-item" ? (
+                    <ol>{block.text}</ol>
+                  ) : (
+                    !block.inlineStyleRanges.length && block.text
+                  )}
+                </div>
+              );
+            }):"" : <div  dangerouslySetInnerHTML={{__html: itinerary}} />
             : ""}
         </div>
       </div>
